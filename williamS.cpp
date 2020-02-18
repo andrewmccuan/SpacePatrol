0d7b86ff7ac81f36f45c108f6e723273be1a313a
Student <student@lab-311-021.cs.csubak.edu>
2020-02-18 09:43:38 -0800

    First file changes

--- williamS.cpp
+++ williamS.cpp
@@ -1,2 +1,15 @@
 // William Source File
 // Created February 2020
+//
+void draw_will_text()
+{
+        Rect r;
+        glClear(GL_COLOR_BUFFER_BIT);
+        //
+        r.bot = gl.yres - 50;
+        r.left = 10;
+        r.center = 0;
+        ggprint8b(&r, 20, 0x00ff0000, "William Sparks - Space Patrol");
+        glPushMatrix();
+}
+
