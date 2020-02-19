2cbfa65b414ed62e3143f9d06fa2327d43bfd975
Student <student@lab-311-023.cs.csubak.edu>
2020-02-19 09:43:55 -0800

    Asteroids

--- raagP.cpp
+++ raagP.cpp
@@ -8,7 +8,7 @@
 void raag_text(int yres, int xres)
 {
     Rect r;
-    r.bot = yres-20;
+    r.bot = yres-65;
     r.left = xres/2;
     r.center = 0;
     ggprint8b(&r,75, 0x4b3ecf, "Raag Patel");
