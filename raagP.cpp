934303e05624dc31d6a86e7bcf9f6566157a00ed
Student <student@lab-311-023.cs.csubak.edu>
2020-02-21 09:31:29 -0800

    changed

--- raagP.cpp
+++ raagP.cpp
@@ -4,7 +4,7 @@
 
 #include <GL/glx.h>
 #include "fonts.h"
-
+/*
 void raag_text(int yres, int xres)
 {
     Rect r;
@@ -12,4 +12,17 @@ void raag_text(int yres, int xres)
     r.left = xres/2;
     r.center = 0;
     ggprint8b(&r,75, 0x4b3ecf, "Raag Patel");
+}*/
+// Friday
+//------------------------------------------------------------------------------------------
+void raag_text(int yres, int xres)
+{
+    Rect r;
+    r.bot = yres-65;
+    r.left = xres/2;
+    r.center = 0;
+    ggprint8b(&r,75, 0x4E9258, "Friday--> The quick brown fox jumps over the lazy dog --> Raag Patel");
+    
 }
+//--------------------------------------------------------------------------------------------
+
